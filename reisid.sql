713eb26d2e3ee7adb90feb947f463807614c1dfb
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2024-09-12 14:23:51 +0300

    Update reisid.sql

--- reisid.sql
+++ reisid.sql
@@ -1,1 +1,53 @@
+create table reisi (
+	reisi_id INT,
+	lähte_linn VARCHAR(50),
+	saabumise_linn VARCHAR(50),
+	valjumis_aeg VARCHAR(50),
+	saabumise_aeg VARCHAR(50),
+	lennujaama_nimi VARCHAR(50),
+	hind INT,
+	transport_hind INT
+);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (1, 'San Jorge', 'Curuzú Cuatiá', '10:44', '13:25', 'Fernando Luis Ribas Dominicci Airport', 422, 115);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (2, 'Rat Burana', 'Zamoskvorech’ye', '13:05', '3:25', 'Hukuntsi Airport', 565, 63);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (3, 'Dagu', 'San Miguel de Cauri', '4:01', '19:58', 'Gualeguaychu Airport', 2149, 81);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (4, 'Singosari', 'Hongor', '15:36', '5:11', 'Cochstedt Airport', 2884, 169);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (5, 'Ibeto', 'Oslo', '8:40', '15:35', 'Ontario Municipal Airport', 1062, 98);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (6, 'Monastyryshche', 'Kedungwungu', '21:09', '11:59', 'Nefteyugansk Airport', 3255, 98);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (7, 'Radashkovichy', 'Uthai', '21:50', '13:53', 'Abilene Regional Airport', 1536, 189);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (8, 'Liyang', 'Manūjān', '6:53', '19:45', 'Pemba Airport', 1644, 188);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (9, 'Ngreco', 'Dongjiao', '9:09', '3:05', 'Elkhart Municipal Airport', 1186, 193);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (10, 'Thị Trấn Thất Khê', 'Briceni', '9:32', '21:23', 'Ukunda Airstrip', 500, 131);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (11, 'Jeblog Satu', 'Katy', '10:11', '12:32', 'Hobart Regional Airport', 2567, 81);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (12, 'Shijia', 'Andapa', '6:00', '6:22', 'Santa Rosa Airport', 2589, 182);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (13, 'Pasirnangka', 'Gœsdorf', '10:53', '3:16', 'Zachar Bay Seaplane Base', 2327, 75);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (14, 'Jixiang', 'Tempurejo', '7:17', '15:40', 'Beverley Springs Airport', 1868, 193);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (15, 'Longmen', 'Danderyd', '23:57', '20:16', 'Frederick Regional Airport', 803, 57);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (16, 'Gasri', 'Willowmore', '15:03', '13:52', 'Paro Airport', 2999, 113);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (17, 'Kurye', 'Åmål', '16:43', '7:55', 'Port Allen Airport', 854, 87);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (18, 'Maharagama', 'Shiraoka', '22:01', '8:07', 'Gustavus Airport', 2414, 65);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (19, 'Banjar Dauhpura', 'Sukarame', '13:24', '19:04', 'DuBois Regional Airport', 1814, 93);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (20, 'Detupia', 'Lazaro Cardenas', '15:40', '18:19', 'Hornepayne Municipal Airport', 2841, 73);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (21, 'Loureiro', 'New York City', '6:38', '19:58', 'Propriá Airport', 1077, 93);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (22, 'Punkalaidun', 'Correntina', '16:52', '4:59', 'Spokane International Airport', 1991, 112);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (23, 'Montemor-o-Velho', 'Brest', '21:12', '6:43', 'King Hussein Air College', 2191, 113);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (24, 'Balice', 'Ishkhoy-Yurt', '22:29', '17:47', 'Scribner State Airport', 464, 189);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (25, 'Gaoshi', 'Manggu', '11:19', '2:19', 'Clayton Municipal Airpark', 3025, 109);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (26, 'Sada', 'Handan', '9:49', '23:33', 'Innsbruck Airport', 1121, 134);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (27, 'Ñuñoa', 'Sliema', '1:58', '21:27', 'Fayetteville Municipal Airport', 1407, 74);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (28, 'Paris 09', 'Cherbourg-Octeville', '19:26', '16:32', 'Cobar Airport', 1375, 94);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (29, 'Đình Lập', 'Borovan', '0:43', '17:32', 'Richards Bay Airport', 2372, 110);
+insert into reisi (reisi_id, lähte_linn, saabumise_linn, valjumis_aeg, saabumise_aeg, lennujaama_nimi, hind, transport_hind) values (30, 'Xishui', 'Gaocheng', '7:54', '19:38', 'Bamburi Airport', 1036, 192);
 
+SELECT
+    reisi_id AS '@reisi_id',
+	hind AS 'Lend/Hind',
+    lähte_linn AS 'Lend/Valjumine/Linn',
+    valjumis_aeg AS 'Lend/Valjumine/Aeg',
+    saabumise_linn AS 'Lend/Saabumine/Linn',
+    saabumise_aeg AS 'Lend/Saabumine/Aeg',
+    lennujaama_nimi AS 'Lend/LennujaamaNimi',
+	transport_hind AS 'Lend/TransportHind'
+	
+FROM reisi
+FOR XML PATH('Reisi'), ROOT('Reisid')
